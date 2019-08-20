9ab8e44ca733c99265c6dcf3ebe77e9ad0ad59e3
Gabriel Lima <gabrielveigalima@icloud.com>
2019-08-20 14:41:27 -0300

    retorna todos usuarios

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+import usuarios
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def home():
     return 'Hello, world!!', 200
+
+@app.route('/usuarios', methods=['GET'])
+def all_users():
+    return jsonify(usuarios.all_users()), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
