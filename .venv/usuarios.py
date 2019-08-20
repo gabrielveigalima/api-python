d2bb96c202dca261028883e50536124ae68a029f
Gabriel Lima <gabrielveigalima@icloud.com>
2019-08-20 14:28:16 -0300

    criando modulo de usuario

---
+++ .venv/usuarios.py
@@ -0,0 +1,18 @@
+
+users = [
+    {
+        'id': 1,
+        'nome': 'Gabriel'
+    }, {
+        'id': 2,
+        'nome': 'João'
+    }, {
+        'id': 3,
+        'nome': 'Paulo'
+    }, {
+        'id': 4,
+        'nome': 'Pedro'
+    }
+]
+def all_users():
+    return users
