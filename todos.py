cb2c05607fd361ffc66d5e44bbe1829ae3d2b8d6
Gabriel Lima <gabrielveigalima@icloud.com>
2019-08-21 11:07:28 -0300

    :sparkles: criando crud to-dos

--- todos.py
+++ todos.py
@@ -1,5 +1,25 @@
+from uuid import uuid4
+from user import User
+
+
 
 #TO-DOS
+class ToDos(object):
 
-class ToDos:
-    pass
+    def __init__(self):
+        self.to_dos = []
+
+    def seletec_all(self):
+        pass
+
+    def select_per_uuid(self, uuid):
+        pass
+
+    def append(self, data):
+        pass
+
+    def update(self, data):
+        pass
+
+    def delete(self, uuid):
+        pass
