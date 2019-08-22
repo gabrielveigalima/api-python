8a65a195091226d390ad89053de9853abf1d66ec
Gabriel Lima <gabrielveigalima@icloud.com>
2019-08-22 15:03:13 -0300

    :zap: add class connect

--- conn.py
+++ conn.py
@@ -1,7 +1,24 @@
 import os
 import simplejson
 from sqlalchemy import create_engine
-import sqlalchemy
+import pymysql
+
 
 class Conn(object):
-    pass
+
+    def __init__(self):
+        with open("./Credentials/conn.json") as fh:
+            self.red_creds = simplejson.loads(fh.read())
+
+        self.connection_string = "mysql+mysqldb://{user}:{passw}@{host}/{db}".format(user=self.red_creds['user'],
+                                                                                           passw=self.red_creds['pass'],
+                                                                                           host=self.red_creds['host'],
+                                                                                           db=self.red_creds['db'])
+        self.db_alias = "{user}@{db}".format(user=self.red_creds['user'], db=self.red_creds['db'])
+
+        pymysql.install_as_MySQLdb()
+
+    def conn(self):
+        # read credentials db
+        self.timescale_engine = create_engine(self.connection_string)
+        print(self.timescale_engine )
